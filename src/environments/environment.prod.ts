242f373f1b5768f4db0f70ffd71057e002084410
Kesavan Anandham <kesavananandham@Admins-Mac-mini.local>
2020-02-11 09:19:16 +0530

    github publish angular

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCSTO7Wo6IPRkj0JFHCDouoGVXl1c4Uus4",
+    authDomain: "data-670f3.firebaseapp.com",
+    databaseURL: "https://data-670f3.firebaseio.com",
+    projectId: "data-670f3",
+    storageBucket: "data-670f3.appspot.com",
+    messagingSenderId: "240506904963",
+    appId: "1:240506904963:web:60134ce30a700b4c7356b6",
+    measurementId: "G-Y8PEQNC7RL"
+  }
 };
